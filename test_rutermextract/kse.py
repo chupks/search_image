e66063a5e561511a4e502e7dc08e20600182c26e
chupks <65542374+chupks@users.noreply.github.com>
2021-06-12 17:49:30 +0500

    Update kse.py

--- test_rutermextract/kse.py
+++ test_rutermextract/kse.py
@@ -1,8 +1,11 @@
-from requests import get
+from requests import get, post
 from rutermextract import TermExtractor
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 te = TermExtractor()
+
+stopterms = ['англ', 'такой образ']
 
 def parse_yandex_referats(url):
     content = get (url) .content
@@ -11,14 +14,42 @@ def parse_yandex_referats(url):
     
     return text
 
-def get_keywords(url):
-    text = parse_yandex_referats(url)
-    keywords = [ x for x in te(text, strings=1) if x.count(" ") > 0 ] 
+def parse_dukcduckgo (term):
+    x = post(
+		'https://html.duckduckgo.com/html/', 
+		data={'q':term.replace(' ', '+')}, 
+		headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
+    ).content
+    dom = BeautifulSoup(x, features="html5lib")
+    snippets = [ x.text for x in dom('a', {'class':'result__snippet'}) ]
+    text = " ".join(snippets)
+
+    return text
+
+def parse_habr (url):
+    content = get (url) .content
+    html = BeautifulSoup (content, features="html5lib")
+    text = html.find ("div", {"class": "post__wrapper"}).text
     
-    print (keywords)
+    return text
+
+def get_url_keywords(url):
+    text = parse_habr(url)    
+    return get_text_keywords(text)
+
+def get_text_keywords(text):
+    keywords = [
+		t for t in te(text, strings=1) if t.count(' ') > 0 and t not in stopterms 
+	]
     
+    return keywords
+
 for url in [
-    "https://yandex.ru/referats/?t=mathematics&s=22798",
-    "https://yandex.ru/referats/?t=mathematics&s=37418",
+    "https://habr.com/ru/post/416889/"
 ]:
-    get_keywords(url)
+    kw = get_url_keywords(url)
+    for term in kw [0 : 3]:
+        text = parse_dukcduckgo (term)
+        kd = get_text_keywords(text)
+        
+        pprint((term, kd))
